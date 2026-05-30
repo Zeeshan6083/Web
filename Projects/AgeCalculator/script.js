31bec8a335078ffa6151c0b85a44464b28675736
Zeeshan608 <abdulzeeshan990@gmail.com>
2026-05-30 20:49:41 +0530

    Fix formatting of code in script.js for improved readability

--- Projects/AgeCalculator/script.js
+++ Projects/AgeCalculator/script.js
@@ -6,7 +6,8 @@ document.querySelector("form").addEventListener("submit", (event) => {
 
   let age = Number(curDate.split("-")[0]) - Number(dob.split("-")[0]);
 
-  document.getElementById("DOB").value = "";
+  document.getElementById
+  ("DOB").value = "";
   document.getElementById("CurrentDate").value = "";
   document.getElementById("MyAge").innerText = age;
   alert("are You Soure")
